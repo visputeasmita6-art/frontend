874d545be9f3bfeaa76bdd213dd19bab61831561
visputeasmita6-art <visputeasmita6@gmail.com>
2026-07-15 10:57:25 +0530

    Changes done for backend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const API = "http://localhost:5000/products";
+  const API = `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/products`;
 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
